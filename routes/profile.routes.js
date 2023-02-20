d6155f9e698c68a0f2b0220c70c67534505a7bdb
Tomás Belmar <tomasbelmar@MacBook-Air-de-Tomas.local>
2023-02-20 12:05:13 +0000

    image background

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -11,16 +11,25 @@ router.get("/profile", (req, res, next) => {
 
 
 router.get("/genarator", (req, res, next) => {
-    
-    res.render("genarator"); // tenho ingredientes 
+    recipies.find()
+    .then (recipiesFromDB => {
+        res.render("genarator"); // tenho ingredientes
+    })
+    .catch (err => console.log ("Error while displaying a form to create a recipie: ", err))
   });
 
   router.post("/genarator", (req, res, next) => {
+    recipies.create (req.body)
+    .then (newRecipie => {
+        res.redirect ("genarator")
+
+    })
+    .catch (err => console.log ("Error while creating a recipie:", err))
     // recebe info do user 
     // faz chamada a api 
     // recebe a info da api 
     // envia para a view.
-        res.render("genarator");
+        
       });
 
 
